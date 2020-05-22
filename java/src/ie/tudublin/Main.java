69624ea556b82002cd32ca6d29f38199041dfa5c
BuildTools <unconfigured@null.spigotmc.org>
2020-05-22 23:55:41 +0100

    last fixes

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,18 +1,9 @@
 package ie.tudublin;
 
-import C18433856.CubeVisual;
-import C18433856.MyVisual;
-import C18433856.RotatingAudioBands;
 import C18433856.myFFT;
 
 public class Main
 {	
-
-	public void startUI()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
-	}
 
 	public void testUI()
 	{
